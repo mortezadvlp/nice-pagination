9cbafece9cecc067404a4510d011e4778b4fcec7
Morteza <mortezadvlp@gmail.com>
2023-03-09 17:48:48 +0330

    Align page numbers

--- example/src/App.js
+++ example/src/App.js
@@ -3,9 +3,20 @@ import { NicePagination } from 'nice-pagination'
 
 const App = () => {
   return (
-    <NicePagination initialPage={1} totalPages={10} minButtonsToDots={6}
-      type='simple'
-      onPageChanged={(p) => console.log("SelectedPage:", p)}  />
+    <div >
+      <NicePagination initialPage={1} totalPages={10} minButtonsToDots={6}
+        type='simple'
+        onPageChanged={(p) => console.log("SelectedPage:", p)}  />
+        
+      <NicePagination initialPage={1} totalPages={10} minButtonsToDots={6}
+        type='square'
+        onPageChanged={(p) => console.log("SelectedPage:", p)}  />
+        
+      <NicePagination initialPage={1} totalPages={10} minButtonsToDots={6}
+        type='circle'
+        onPageChanged={(p) => console.log("SelectedPage:", p)}  />
+    </div>
+    
   );
 }
 
